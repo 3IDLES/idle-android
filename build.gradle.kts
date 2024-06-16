3cc491696fe4aaae069bc7f073aa1b9a92de87d8
tgyuu-An <atg0614@gmail.com>
2024-06-17 02:23:06 +0900

    chore #1 : ktlint 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.ktlint)
 }
