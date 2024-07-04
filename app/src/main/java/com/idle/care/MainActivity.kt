335de4bcc2a67a94d94bca9b2f071da7f804ee1f
tgyuu-An <atg0614@gmail.com>
2024-07-04 10:08:06 +0900

    [IDLE-124] BottomNavigationView Visible/InVisibile 핸들링 로직 추가

--- app/src/main/java/com/idle/care/MainActivity.kt
+++ app/src/main/java/com/idle/care/MainActivity.kt
@@ -2,6 +2,7 @@ package com.idle.care
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.idle.care.databinding.ActivityMainBinding
@@ -21,6 +22,27 @@ class MainActivity : AppCompatActivity() {
             val navHostFragment =
                 supportFragmentManager.findFragmentById(R.id.main_FCV) as NavHostFragment
             navController = navHostFragment.navController
+        }
+
+        setDestinationListener()
+    }
+
+    private val hideBottomNavDestinationIds: Set<Int> by lazy {
+        resources.obtainTypedArray(R.array.hideBottomNavDestinationIds).let { typedArray ->
+            val destinationIds = mutableSetOf<Int>()
+
+            for (index in 0 until typedArray.length()) {
+                destinationIds.add(typedArray.getResourceId(index, 0))
+            }
+
+            typedArray.recycle()
+            destinationIds
+        }
+    }
+
+    private fun setDestinationListener() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.mainBNV.isVisible = (destination.id !in hideBottomNavDestinationIds)
         }
     }
 }
