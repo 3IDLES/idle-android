a932b4346bc73013563999539d94e606a43181f1
tgyuuAn <atg0614@gmail.com>
2024-07-07 15:06:31 +0900

    [IDLE-79] Error Response 정의 및 onResponse 에서 에러메세지를 던지도록 정의

--- core/network/src/main/java/com/idle/network/util/onResponse.kt
+++ core/network/src/main/java/com/idle/network/util/onResponse.kt
@@ -1,14 +1,23 @@
 package com.idle.network.util
 
+import com.idle.network.model.error.ErrorResponse
+import kotlinx.serialization.json.Json
 import retrofit2.Response
 
-fun <T> Response<T>.onResponse(): Result<T> {
+internal fun <T> Response<T>.onResponse(): Result<T> {
     if (isSuccessful) {
         body()?.let { body ->
             return Result.success(body)
         } ?: return Result.failure(Throwable("Response Body is Null"))
     } else {
-        return Result.failure(errorBody()?.string()?.let { Throwable(it) }
-            ?: Throwable("Response is Failure"))
+        val errorResponse = errorBody()?.string()?.let { errorBodyString ->
+            try {
+                Json.decodeFromString<ErrorResponse>(errorBodyString)
+            } catch (e: Exception) {
+                ErrorResponse()
+            }
+        } ?: ErrorResponse()
+
+        return Result.failure(Throwable(errorResponse.message))
     }
 }
