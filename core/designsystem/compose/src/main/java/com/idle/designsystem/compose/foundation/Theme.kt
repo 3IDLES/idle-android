bd7383d76167514af36483fb3170c0d0d83bc7ac
tgyuuAn <atg0614@gmail.com>
2024-07-08 11:27:27 +0900

    [IDLE-75] Color, Typography 정의 및 Foundation 완성

--- core/designsystem/compose/src/main/java/com/idle/designsystem/compose/foundation/Theme.kt
+++ core/designsystem/compose/src/main/java/com/idle/designsystem/compose/foundation/Theme.kt
@@ -1,57 +1,28 @@
 package com.idle.designsystem.compose.foundation
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+val LocalColors = staticCompositionLocalOf { CareColors() }
+val LocalTypography = staticCompositionLocalOf { CareTypography() }
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
-
+// Color Scheme이 정의 되지 않았고, dark,light 별 색상이 정의 되지 않아서 아직은 사용 하지 않았습니다!
 @Composable
 fun CareTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    CompositionLocalProvider {
+        MaterialTheme(
+            content = content
+        )
     }
+}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+object CareTheme {
+    val colors: CareColors
+        @Composable get() = LocalColors.current
+    val typography: CareTypography
+        @Composable get() = LocalTypography.current
 }
