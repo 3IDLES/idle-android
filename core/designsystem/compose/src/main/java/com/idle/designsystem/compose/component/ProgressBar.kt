c99b736187f0c72bd5008dfe181ebd957c352962
tgyuuAn <atg0614@gmail.com>
2024-07-08 14:24:11 +0900

    [IDLE-134] ProgressBar 컴포넌트 추가

---
+++ core/designsystem/compose/src/main/java/com/idle/designsystem/compose/component/ProgressBar.kt
@@ -0,0 +1,75 @@
+package com.idle.designsystem.compose.component
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults.ProgressAnimationSpec
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.idle.designsystem.compose.foundation.CareTheme
+
+@Composable
+fun CareProgressBar(
+    currentStep: Int,
+    totalSteps: Int,
+    modifier: Modifier = Modifier,
+) {
+    val progress by animateFloatAsState(
+        targetValue = (currentStep / totalSteps.toFloat()),
+        animationSpec = ProgressAnimationSpec,
+    )
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        LinearProgressIndicator(
+            progress = { progress },
+            color = CareTheme.colors.orange500,
+            trackColor = CareTheme.colors.gray100,
+            strokeCap = StrokeCap.Round,
+            modifier = Modifier
+                .padding(vertical = 10.dp)
+                .height(4.dp)
+                .weight(1f),
+        )
+
+        Text(
+            text = formatProgressText(currentStep, totalSteps),
+            style = CareTheme.typography.subtitle3,
+        )
+    }
+}
+
+@Composable
+private fun formatProgressText(
+    currentStep: Int,
+    totalSteps: Int,
+) = buildAnnotatedString {
+    withStyle(
+        style = SpanStyle(color = CareTheme.colors.orange500,)
+    ) {
+        append("$currentStep ")
+    }
+    withStyle(
+        style = SpanStyle(
+            color = CareTheme.colors.gray300,
+            fontSize = 14.sp,
+        )
+    ) {
+        append("/ $totalSteps")
+    }
+}
