77fb5908a9b171822cc5d9a07e64cec0f3b41470
tgyuuAn <atg0614@gmail.com>
2024-07-08 11:27:27 +0900

    [IDLE-75] Color, Typography 정의 및 Foundation 완성

--- core/designsystem/compose/src/main/java/com/idle/designsystem/compose/foundation/Color.kt
+++ core/designsystem/compose/src/main/java/com/idle/designsystem/compose/foundation/Color.kt
@@ -1,11 +1,44 @@
 package com.idle.designsystem.compose.foundation
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange100 = Color(0xff131417)
+val Orange200 = Color(0xffFFCA99)
+val Orange300 = Color(0xffFFAF66)
+val Orange400 = Color(0xffFF9533)
+val Orange500 = Color(0xffFF7A00)
+val Orange600 = Color(0xffCC6200)
+val Orange700 = Color(0xff994900)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Gray050 = Color(0xffF1F2F4)
+val Gray100 = Color(0xffDFE0E3)
+val Gray200 = Color(0xffBFC1C7)
+val Gray300 = Color(0xff9EA3AB)
+val Gray400 = Color(0xff7E848F)
+val Gray500 = Color(0xff5E6573)
+val Gray600 = Color(0xff4B515C)
+val Gray700 = Color(0xff383D45)
+val Gray800 = Color(0xff26282E)
+val Gray900 = Color(0xff131417)
+
+@Immutable
+data class CareColors(
+    val orange100: Color = Orange100,
+    val orange200: Color = Orange200,
+    val orange300: Color = Orange300,
+    val orange400: Color = Orange400,
+    val orange500: Color = Orange500,
+    val orange600: Color = Orange600,
+    val orange700: Color = Orange700,
+    val gray050: Color = Gray050,
+    val gray100: Color = Gray100,
+    val gray200: Color = Gray200,
+    val gray300: Color = Gray300,
+    val gray400: Color = Gray400,
+    val gray500: Color = Gray500,
+    val gray600: Color = Gray600,
+    val gray700: Color = Gray700,
+    val gray800: Color = Gray800,
+    val gray900: Color = Gray900
+)
---
+++ core/designsystem/compose/src/main/java/com/idle/designsystem/compose/foundation/Typography.kt
@@ -0,0 +1,106 @@
+package com.idle.designsystem.compose.foundation
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.idle.designsystem.compose.R
+
+val PretendardBold = FontFamily(
+    Font(
+        resId = R.font.pretendard_bold,
+        weight = FontWeight.Bold,
+    ),
+)
+
+val PretendardSemiBold = FontFamily(
+    Font(
+        resId = R.font.pretendard_semi_bold,
+        weight = FontWeight.SemiBold,
+    ),
+)
+
+val PretendardMedium = FontFamily(
+    Font(
+        resId = R.font.pretendard_medium,
+        weight = FontWeight.Medium,
+    ),
+)
+
+@Immutable
+data class CareTypography(
+    val heading1: TextStyle = TextStyle(
+        fontFamily = PretendardBold,
+        fontSize = 24.sp,
+        lineHeight = 34.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val heading2: TextStyle = TextStyle(
+        fontFamily = PretendardBold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val heading3: TextStyle = TextStyle(
+        fontFamily = PretendardBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val heading4: TextStyle = TextStyle(
+        fontFamily = PretendardBold,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val subtitle1: TextStyle = TextStyle(
+        fontFamily = PretendardSemiBold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val subtitle2: TextStyle = TextStyle(
+        fontFamily = PretendardSemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val subtitle3: TextStyle = TextStyle(
+        fontFamily = PretendardSemiBold,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val subtitle4: TextStyle = TextStyle(
+        fontFamily = PretendardSemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val body1: TextStyle = TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val body2: TextStyle = TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val body3: TextStyle = TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = (-0.2).sp,
+    ),
+    val caption: TextStyle = TextStyle(
+        fontFamily = PretendardMedium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = (-0.2).sp,
+    )
+)
