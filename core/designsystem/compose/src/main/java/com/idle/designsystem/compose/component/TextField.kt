7611aa957bc85faba2278c9a2b7882d6bea4ce61
tgyuuAn <atg0614@gmail.com>
2024-07-10 15:56:12 +0900

    [IDLE-134] 외부 화면을 클릭시 키보드를 내리도록 하는 Modifier Util 추가

--- core/designsystem/compose/src/main/java/com/idle/designsystem/compose/component/TextField.kt
+++ core/designsystem/compose/src/main/java/com/idle/designsystem/compose/component/TextField.kt
@@ -3,7 +3,7 @@ package com.idle.designsystem.compose.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -11,6 +11,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
@@ -26,32 +27,22 @@ fun CareTextField(
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
 
-    BasicTextField(
-        value = value,
-        onValueChange = onValueChanged,
-        textStyle = CareTheme.typography.body3,
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(
-            imeAction = ImeAction.Done,
-        ),
-        keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
-    ) { innerTextField ->
-        Box(
-            modifier = modifier
-                .background(color = CareTheme.colors.white000, shape = RoundedCornerShape(6.dp))
-                .border(
-                    width = 1.dp,
-                    color = CareTheme.colors.gray100,
-                    shape = RoundedCornerShape(6.dp)
-                ),
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp, horizontal = 16.dp),
-            ) {
-                innerTextField()
-
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = modifier
+            .height(44.dp)
+            .background(color = CareTheme.colors.white000, shape = RoundedCornerShape(6.dp))
+            .border(width = 1.dp, color = CareTheme.colors.gray100, shape = RoundedCornerShape(6.dp))
+            .padding(horizontal = 16.dp, vertical = 10.dp)
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChanged,
+            textStyle = CareTheme.typography.body3,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
+            decorationBox = { innerTextField ->
                 if (value.isEmpty()) {
                     Text(
                         text = hint,
@@ -59,7 +50,8 @@ fun CareTextField(
                         color = CareTheme.colors.gray200,
                     )
                 }
+                innerTextField()
             }
-        }
+        )
     }
 }
