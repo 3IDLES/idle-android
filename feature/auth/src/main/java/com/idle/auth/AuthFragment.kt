21a0514d0618435cd2803e72ef5159df03d0f701
tgyuuAn <atg0614@gmail.com>
2024-07-11 12:48:14 +0900

    [IDLE-134] 가장 처음 Auth화면 구현

--- feature/auth/src/main/java/com/idle/auth/AuthFragment.kt
+++ feature/auth/src/main/java/com/idle/auth/AuthFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -27,6 +31,7 @@ import com.idle.binding.DeepLinkDestination.CenterAuth
 import com.idle.binding.DeepLinkDestination.WorkerAuth
 import com.idle.binding.deepLinkNavigateTo
 import com.idle.binding.repeatOnStarted
+import com.idle.designsystem.compose.foundation.CareTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -77,24 +82,68 @@ internal fun AuthScreen(
     navigateToCenterAuth: () -> Unit = {},
 ) {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
         modifier = Modifier.fillMaxSize()
             .background(Color.White)
             .padding(horizontal = 20.dp),
     ) {
-        Spacer(
-            modifier = Modifier.height(300.dp)
-                .fillMaxWidth()
-                .background(Color.Black)
+        Text(
+            text = "어플 소개 한 줄 정도\n그리고 어플 이름",
+            style = CareTheme.typography.heading1.copy(fontSize = 28.sp),
+            modifier = Modifier.padding(top = 108.dp),
         )
 
-        Button(onClick = navigateToWorkerAuth) {
-            Text(text = "요양 보호사로 시작하기")
-        }
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 130.dp, bottom = 200.dp),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 16.dp,
+                    alignment = Alignment.CenterVertically
+                ),
+                modifier = Modifier
+                    .height(208.dp)
+                    .weight(1f)
+                    .border(
+                        width = 1.dp,
+                        color = CareTheme.colors.gray100,
+                        shape = RoundedCornerShape(8.dp)
+                    ).clickable { navigateToCenterAuth() }
+                    .padding(horizontal = 25.5.dp, vertical = 37.dp),
+            ) {
+                Text(
+                    text = "센터 관리자로\n시작하기",
+                    style = CareTheme.typography.subtitle2,
+                    color = CareTheme.colors.gray900,
+                )
+            }
 
-        Button(onClick = navigateToCenterAuth) {
-            Text(text = "센터 관리자로 시작하기")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 16.dp,
+                    alignment = Alignment.CenterVertically
+                ),
+                modifier = Modifier
+                    .height(208.dp)
+                    .weight(1f)
+                    .border(
+                        width = 1.dp,
+                        color = CareTheme.colors.gray100,
+                        shape = RoundedCornerShape(8.dp)
+                    ).clickable { navigateToWorkerAuth() }
+                    .padding(horizontal = 25.5.dp, vertical = 37.dp),
+            ) {
+                Text(
+                    text = "요양 보호사로\n시작하기",
+                    style = CareTheme.typography.subtitle2,
+                    color = CareTheme.colors.gray900,
+                )
+            }
         }
     }
 }
