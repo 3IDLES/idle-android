8cd3dd427fd6484bea195908c380e5ecfa7f0e31
tgyuuAn <atg0614@gmail.com>
2024-07-12 11:11:11 +0900

    [IDLE-134] BaseComposeFragment 추가

---
+++ core/common-ui/compose/src/main/java/com/idle/compose/base/BaseComposeFragment.kt
@@ -0,0 +1,37 @@
+package com.idle.compose.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+abstract class BaseComposeFragment : Fragment() {
+
+    abstract val viewModel: ViewModel
+
+    private lateinit var composeView: ComposeView
+
+    @Composable
+    abstract fun ComposeLayout()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ComposeView(requireContext()).also {
+            composeView = it
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        composeView.setContent {
+            ComposeLayout()
+        }
+    }
+}
