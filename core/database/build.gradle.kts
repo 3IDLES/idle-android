ba31058a9b5bb3add41bbb1affeb7fed9d77e6bb
tgyuuAn <atg0614@gmail.com>
2024-07-13 09:50:55 +0900

    [IDLE-80] Local DB 모듈 database 추가

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("care.android.library")
+    id("care.android.hilt")
+}
+
+android {
+    namespace = "com.idle.database"
+}
+
+dependencies {
+}
--- core/network/src/main/java/com/idle/network/util/onResponse.kt
+++ core/network/src/main/java/com/idle/network/util/onResponse.kt
@@ -1,23 +1,12 @@
 package com.idle.network.util
 
-import com.idle.network.model.error.ErrorResponse
-import kotlinx.serialization.json.Json
 import retrofit2.Response
 
 internal fun <T> Response<T>.onResponse(): Result<T> {
     if (isSuccessful) {
-        body()?.let { body ->
-            return Result.success(body)
-        } ?: return Result.success(Unit as T)
+        return Result.success(body()!!)
     } else {
-        val errorResponse = errorBody()?.string()?.let { errorBodyString ->
-            try {
-                Json.decodeFromString<ErrorResponse>(errorBodyString)
-            } catch (e: Exception) {
-                ErrorResponse()
-            }
-        } ?: ErrorResponse()
-
-        return Result.failure(Throwable(errorResponse.message))
+        val errorBody = errorBody()
+        throw IllegalArgumentException(errorBody.toString())
     }
 }
