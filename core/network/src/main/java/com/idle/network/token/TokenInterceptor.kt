de047e97ca7c52befd46cf49dc42437d6d158db3
tgyuuAn <atg0614@gmail.com>
2024-07-13 21:20:45 +0900

    [IDLE-80] 특정 API를 제외하고 토큰을 담는 로직 추가

--- core/network/src/main/java/com/idle/network/token/TokenInterceptor.kt
+++ core/network/src/main/java/com/idle/network/token/TokenInterceptor.kt
@@ -1,6 +1,7 @@
 package com.idle.network.token
 
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import javax.inject.Inject
 
@@ -9,6 +10,32 @@ class TokenInterceptor @Inject constructor(
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        return chain.proceed(request)
+        val authRequest = if (isAccessTokenUsed(request)) {
+            request.newBuilder()
+                .addHeader("Authorization", tokenProvider.getAccessToken())
+                .build()
+        } else {
+            request
+        }
+        return chain.proceed(authRequest)
+    }
+
+    private fun isAccessTokenUsed(request: Request): Boolean {
+        if (request.url.encodedPath.contains("/api/v1/auth/center/validation/")) {
+            return false
+        }
+
+        if (request.url.encodedPath.contains("/api/v1/auth/center/authentication/")) {
+            return false
+        }
+
+        return when (request.url.encodedPath) {
+            "/api/v1/auth/common/send" -> false
+            "/api/v1/auth/common/confirm" -> false
+            "/api/v1/auth/center/join" -> false
+            "/api/v1/auth/center/login" -> false
+            "/api/v1/auth/center/refresh" -> false
+            else -> true
+        }
     }
 }
