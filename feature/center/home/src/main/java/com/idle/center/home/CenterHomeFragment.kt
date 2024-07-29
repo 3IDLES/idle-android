166a537d967cb3e76619d7ddcf96dcf60b91c7f6
tgyuuAn <atg0614@gmail.com>
2024-07-29 17:34:36 +0900

    [IDLE-106] 센터 공고 전체 조회 화면 UI 완성

--- feature/center/home/src/main/java/com/idle/center/home/CenterHomeFragment.kt
+++ feature/center/home/src/main/java/com/idle/center/home/CenterHomeFragment.kt
@@ -19,12 +19,14 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.idle.compose.base.BaseComposeFragment
 import com.idle.compose.clickable
 import com.idle.designresource.R
@@ -40,13 +42,19 @@ internal class CenterHomeFragment : BaseComposeFragment() {
     @Composable
     override fun ComposeLayout() {
         fragmentViewModel.apply {
-            CenterHomeScreen()
+            val recruitmentPostStatus by recruitmentPostStatus.collectAsStateWithLifecycle()
+            CenterHomeScreen(
+                recruitmentPostStatus = recruitmentPostStatus,
+                setRecruitmentPostStatus = ::setRecruitmentPostStatus
+            )
         }
     }
 }
 
 @Composable
 internal fun CenterHomeScreen(
+    recruitmentPostStatus: RecruitmentPostStatus,
+    setRecruitmentPostStatus: (RecruitmentPostStatus) -> Unit,
 ) {
     Scaffold(
         topBar = {
@@ -66,34 +74,45 @@ internal fun CenterHomeScreen(
             modifier = Modifier.padding(paddingValue)
                 .fillMaxSize()
         ) {
-            Row {
+            Row(modifier = Modifier.padding(bottom = 20.dp)) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.weight(1f)
-                        .padding(bottom = 20.dp),
+                        .clickable { setRecruitmentPostStatus(RecruitmentPostStatus.ONGOING) },
                 ) {
                     Text(
-                        text = "지원한 공고",
+                        text = "진행 중인 공고",
                         style = CareTheme.typography.subtitle3,
-                        color = CareTheme.colors.gray900,
+                        color = if (recruitmentPostStatus == RecruitmentPostStatus.ONGOING) CareTheme.colors.gray900
+                        else CareTheme.colors.gray300,
                         modifier = Modifier.padding(vertical = 12.dp),
                     )
 
-                    HorizontalDivider(thickness = 2.dp, color = CareTheme.colors.gray900)
+                    if (recruitmentPostStatus == RecruitmentPostStatus.ONGOING) {
+                        HorizontalDivider(thickness = 2.dp, color = CareTheme.colors.gray900)
+                    } else {
+                        HorizontalDivider(thickness = 1.dp, color = CareTheme.colors.gray100)
+                    }
                 }
 
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.weight(1f)
+                        .clickable { setRecruitmentPostStatus(RecruitmentPostStatus.PREVIOUS) },
                 ) {
                     Text(
-                        text = "찜한 공고",
+                        text = "이전 공고",
                         style = CareTheme.typography.subtitle3,
-                        color = CareTheme.colors.gray300,
+                        color = if (recruitmentPostStatus == RecruitmentPostStatus.PREVIOUS) CareTheme.colors.gray900
+                        else CareTheme.colors.gray300,
                         modifier = Modifier.padding(vertical = 12.dp),
                     )
 
-                    HorizontalDivider(thickness = 1.dp, color = CareTheme.colors.gray100)
+                    if (recruitmentPostStatus == RecruitmentPostStatus.PREVIOUS) {
+                        HorizontalDivider(thickness = 2.dp, color = CareTheme.colors.gray900)
+                    } else {
+                        HorizontalDivider(thickness = 1.dp, color = CareTheme.colors.gray100)
+                    }
                 }
             }
 
@@ -102,93 +121,7 @@ internal fun CenterHomeScreen(
                 modifier = Modifier.padding(start = 20.dp, end = 20.dp),
             ) {
                 items(listOf(1, 2, 3, 4, 5)) {
-                    Card(
-                        shape = RoundedCornerShape(12.dp),
-                        colors = CardColors(
-                            containerColor = CareTheme.colors.white000,
-                            contentColor = CareTheme.colors.white000,
-                            disabledContainerColor = CareTheme.colors.white000,
-                            disabledContentColor = CareTheme.colors.white000,
-                        ),
-                        border = BorderStroke(width = 1.dp, color = CareTheme.colors.gray100),
-                        modifier = Modifier.clickable { }
-                    ) {
-                        Column(modifier = Modifier.padding(16.dp)) {
-                            Text(
-                                text = "2024. 07. 10 ~ 2024. 07. 31",
-                                style = CareTheme.typography.body3,
-                                color = CareTheme.colors.gray300,
-                                overflow = TextOverflow.Clip,
-                                maxLines = 1,
-                                modifier = Modifier.padding(bottom = 4.dp),
-                            )
-
-                            Text(
-                                text = "서울특별시 강남구 신사동",
-                                style = CareTheme.typography.subtitle2,
-                                color = CareTheme.colors.gray900,
-                                overflow = TextOverflow.Clip,
-                                maxLines = 1,
-                                modifier = Modifier.padding(bottom = 2.dp),
-                            )
-
-                            Text(
-                                text = "홍길동 | 1등급 78세 여성",
-                                style = CareTheme.typography.body2,
-                                color = CareTheme.colors.gray500,
-                                modifier = Modifier.padding(bottom = 12.dp)
-                            )
-
-                            Row(
-                                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.fillMaxWidth()
-                                    .padding(bottom = 8.dp),
-                            ) {
-                                CareButtonCardMedium(
-                                    text = "지원자 2명 조회",
-                                    onClick = {},
-                                    modifier = Modifier.weight(1f),
-                                )
-
-                                CareButtonCardMedium(
-                                    text = "요양보호사 찾기",
-                                    onClick = {},
-                                    modifier = Modifier.weight(1f),
-                                )
-                            }
-
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                Image(
-                                    painter = painterResource(R.drawable.ic_edit_pencil_non_background),
-                                    contentDescription = null,
-                                    modifier = Modifier.padding(end = 2.dp),
-                                )
-
-                                Text(
-                                    text = "공고 수정",
-                                    style = CareTheme.typography.body3,
-                                    color = CareTheme.colors.gray500,
-                                    modifier = Modifier.padding(end = 4.dp),
-                                )
-
-                                Image(
-                                    painter = painterResource(R.drawable.ic_check_gray),
-                                    contentDescription = null,
-                                    modifier = Modifier.padding(end = 2.dp),
-                                )
-
-                                Text(
-                                    text = "채용 종료",
-                                    style = CareTheme.typography.body3,
-                                    color = CareTheme.colors.gray500,
-                                )
-                            }
-                        }
-                    }
+                    CenterRecruitmentCardCard()
                 }
 
                 item {
@@ -197,6 +130,97 @@ internal fun CenterHomeScreen(
                             .height(20.dp)
                     )
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CenterRecruitmentCardCard() {
+    Card(
+        shape = RoundedCornerShape(12.dp),
+        colors = CardColors(
+            containerColor = CareTheme.colors.white000,
+            contentColor = CareTheme.colors.white000,
+            disabledContainerColor = CareTheme.colors.white000,
+            disabledContentColor = CareTheme.colors.white000,
+        ),
+        border = BorderStroke(width = 1.dp, color = CareTheme.colors.gray100),
+        modifier = Modifier.clickable { }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "2024. 07. 10 ~ 2024. 07. 31",
+                style = CareTheme.typography.body3,
+                color = CareTheme.colors.gray300,
+                overflow = TextOverflow.Clip,
+                maxLines = 1,
+                modifier = Modifier.padding(bottom = 4.dp),
+            )
+
+            Text(
+                text = "서울특별시 강남구 신사동",
+                style = CareTheme.typography.subtitle2,
+                color = CareTheme.colors.gray900,
+                overflow = TextOverflow.Clip,
+                maxLines = 1,
+                modifier = Modifier.padding(bottom = 2.dp),
+            )
+
+            Text(
+                text = "홍길동 | 1등급 78세 여성",
+                style = CareTheme.typography.body2,
+                color = CareTheme.colors.gray500,
+                modifier = Modifier.padding(bottom = 12.dp)
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(bottom = 8.dp),
+            ) {
+                CareButtonCardMedium(
+                    text = "지원자 2명 조회",
+                    onClick = {},
+                    modifier = Modifier.weight(1f),
+                )
+
+                CareButtonCardMedium(
+                    text = "요양 보호사 찾기",
+                    onClick = {},
+                    modifier = Modifier.weight(1f),
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_edit_pencil_non_background),
+                    contentDescription = null,
+                    modifier = Modifier.padding(end = 2.dp),
+                )
+
+                Text(
+                    text = "공고 수정",
+                    style = CareTheme.typography.body3,
+                    color = CareTheme.colors.gray500,
+                    modifier = Modifier.padding(end = 4.dp),
+                )
+
+                Image(
+                    painter = painterResource(R.drawable.ic_check_gray),
+                    contentDescription = null,
+                    modifier = Modifier.padding(end = 2.dp),
+                )
+
+                Text(
+                    text = "채용 종료",
+                    style = CareTheme.typography.body3,
+                    color = CareTheme.colors.gray500,
+                )
             }
         }
     }
--- feature/center/home/src/main/java/com/idle/center/home/CenterHomeViewModel.kt
+++ feature/center/home/src/main/java/com/idle/center/home/CenterHomeViewModel.kt
@@ -2,8 +2,21 @@ package com.idle.center.home
 
 import com.idle.binding.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class CenterHomeViewModel @Inject constructor() : BaseViewModel() {
+    private val _recruitmentPostStatus = MutableStateFlow(RecruitmentPostStatus.ONGOING)
+    val recruitmentPostStatus = _recruitmentPostStatus.asStateFlow()
+
+    internal fun setRecruitmentPostStatus(recruitmentPostStatus: RecruitmentPostStatus){
+        _recruitmentPostStatus.value = recruitmentPostStatus
+    }
+}
+
+enum class RecruitmentPostStatus {
+    ONGOING,
+    PREVIOUS
 }
