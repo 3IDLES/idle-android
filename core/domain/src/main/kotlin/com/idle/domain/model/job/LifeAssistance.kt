153932b24e06da254734db14c1ae010b2f1776c0
tgyuuAn <atg0614@gmail.com>
2024-07-31 10:49:31 +0900

    [IDLE-105] LifeAssistance 추가

---
+++ core/domain/src/main/kotlin/com/idle/domain/model/job/LifeAssistance.kt
@@ -0,0 +1,17 @@
+package com.idle.domain.model.job
+
+import com.idle.domain.model.job.MentalStatus.UNKNOWN
+
+enum class LifeAssistance(val displayName: String) {
+    CLEANING("청소"),
+    LAUNDRY("빨래"),
+    WALKING("산책"),
+    HEALTH("운동보조"),
+    TALKING("말벗");
+
+    companion object {
+        fun create(lifeAssistance: List<String>): List<LifeAssistance> {
+            return LifeAssistance.entries.filter { it.name in lifeAssistance }
+        }
+    }
+}
