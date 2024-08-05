d1a4e01532ca0186348457ca6959d833b563c505
tgyuuAn <atg0614@gmail.com>
2024-08-06 00:02:13 +0900

    [IDLE-000] TokenAuthenticator의 순환 의존을 끊기위해 Retrofit2을 2개 사용하던 거 Provider를 사용해서 순환 의존을 끊는 것으로 리팩토링

--- core/network/src/main/java/com/idle/network/di/RetrofitModule.kt
+++ core/network/src/main/java/com/idle/network/di/RetrofitModule.kt
@@ -2,7 +2,6 @@ package com.idle.network.di
 
 import com.idle.network.BuildConfig
 import com.idle.network.api.CareNetworkApi
-import com.idle.network.api.TokenNetworkApi
 import com.idle.network.token.TokenAuthenticator
 import com.idle.network.token.TokenInterceptor
 import dagger.Module
@@ -15,7 +14,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
-import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -28,7 +26,6 @@ object RetrofitModule {
 
     @Singleton
     @Provides
-    @Named("common")
     fun provideOkHttpClient(
         tokenInterceptor: TokenInterceptor,
         tokenAuthenticator: TokenAuthenticator,
@@ -48,39 +45,12 @@ object RetrofitModule {
 
     @Singleton
     @Provides
-    @Named("token")
-    fun provideTokenOkHttpClient(): OkHttpClient {
-        val builder = OkHttpClient.Builder()
-
-        if (BuildConfig.DEBUG) {
-            val loggingInterceptor = HttpLoggingInterceptor()
-            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-            builder.addInterceptor(loggingInterceptor)
-        }
-
-        return builder.build()
-    }
-
-    @Singleton
-    @Provides
     fun providesCareNetworkApi(
-        @Named("common") okHttpClient: OkHttpClient,
+        okHttpClient: OkHttpClient,
     ): CareNetworkApi = Retrofit.Builder()
         .client(okHttpClient)
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(BuildConfig.CARE_BASE_URL)
         .build()
         .create(CareNetworkApi::class.java)
-
-
-    @Singleton
-    @Provides
-    fun providesTokenNetworkApi(
-        @Named("token") okHttpClient: OkHttpClient,
-    ): TokenNetworkApi = Retrofit.Builder()
-        .client(okHttpClient)
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(BuildConfig.CARE_BASE_URL)
-        .build()
-        .create(TokenNetworkApi::class.java)
 }
