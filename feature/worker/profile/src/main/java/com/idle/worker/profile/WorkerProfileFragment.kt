71d652436998d5169f94334732e97922f4892f8a
tgyuuAn <atg0614@gmail.com>
2024-08-09 17:10:50 +0900

    [IDLE-273] 요양 보호사 프로필 화면 Component 최대한 재사용하도록 변경

--- feature/worker/profile/src/main/java/com/idle/worker/profile/WorkerProfileFragment.kt
+++ feature/worker/profile/src/main/java/com/idle/worker/profile/WorkerProfileFragment.kt
@@ -13,13 +13,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
@@ -33,7 +30,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -42,11 +38,12 @@ import com.idle.compose.base.BaseComposeFragment
 import com.idle.compose.clickable
 import com.idle.designresource.R
 import com.idle.designsystem.compose.component.CareButtonRound
-import com.idle.designsystem.compose.component.CareChipBasic
+import com.idle.designsystem.compose.component.CareClickableTextField
 import com.idle.designsystem.compose.component.CareSubtitleTopBar
 import com.idle.designsystem.compose.component.CareTag
 import com.idle.designsystem.compose.component.CareTextField
 import com.idle.designsystem.compose.component.CareTextFieldLong
+import com.idle.designsystem.compose.component.LabeledContent
 import com.idle.designsystem.compose.foundation.CareTheme
 import com.idle.domain.model.auth.Gender
 import com.idle.domain.model.profile.JobSearchStatus.NO
@@ -121,8 +118,11 @@ internal fun WorkerProfileScreen(
                 ),
             ) {
                 CareSubtitleTopBar(
-                    title = "내 프로필",
-                    onNavigationClick = { onBackPressedDispatcher?.onBackPressed() },
+                    title = stringResource(id = R.string.my_profile),
+                    onNavigationClick = {
+                        if (isEditState) setEditState(false)
+                        else onBackPressedDispatcher?.onBackPressed()
+                    },
                     leftComponent = {
                         if (isEditState) {
                             Text(
@@ -142,7 +142,6 @@ internal fun WorkerProfileScreen(
                     },
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(32.dp)
                         .padding(start = 12.dp, top = 48.dp, end = 20.dp, bottom = 12.dp),
                 )
             }
@@ -154,13 +153,13 @@ internal fun WorkerProfileScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .padding(top = 24.dp)
                 .verticalScroll(scrollState),
         ) {
             if (!isEditState) {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
+                        .align(Alignment.TopCenter)
                         .background(CareTheme.colors.gray050)
                         .height(92.dp)
                 )
@@ -174,25 +173,24 @@ internal fun WorkerProfileScreen(
                     .align(Alignment.TopCenter)
             ) {
                 Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                    horizontalArrangement = Arrangement.Center,
                     verticalAlignment = Alignment.Bottom,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 20.dp)
                 ) {
-                    Spacer(modifier = Modifier.size(24.dp))
-
-                    Box(modifier = Modifier.clickable {
-                        if (isEditState) singlePhotoPickerLauncher.launch(
-                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                        )
-                    }) {
+                    Box(
+                        modifier = Modifier
+                            .clickable(enabled = isEditState) {
+                                singlePhotoPickerLauncher.launch(
+                                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                                )
+                            },
+                    ) {
                         Image(
-                            painter = painterResource(com.idle.designresource.R.drawable.ic_worker_empty),
+                            painter = painterResource(R.drawable.ic_worker_empty),
                             contentDescription = null,
-                            modifier = Modifier
-                                .align(Alignment.Center)
-                                .padding(horizontal = 7.dp),
+                            modifier = Modifier.align(Alignment.Center),
                         )
 
                         if (isEditState) {
@@ -203,12 +201,6 @@ internal fun WorkerProfileScreen(
                             )
                         }
                     }
-
-                    Image(
-                        painter = painterResource(com.idle.designresource.R.drawable.ic_star_gray),
-                        contentDescription = null,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
                 }
 
                 if (!isEditState) {
@@ -279,9 +271,7 @@ internal fun WorkerProfileScreen(
                 if (!isEditState) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .padding(bottom = 28.dp)
-                            .height(IntrinsicSize.Min)
+                        modifier = Modifier.height(IntrinsicSize.Min),
                     ) {
                         Text(
                             text = stringResource(id = R.string.age),
@@ -336,120 +326,42 @@ internal fun WorkerProfileScreen(
                             )
                         }
                     }
-                } else {
-
-                    Column(
-                        horizontalAlignment = Alignment.Start,
-                        verticalArrangement = Arrangement.spacedBy(6.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 20.dp, end = 20.dp, bottom = 28.dp),
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.gender),
-                            style = CareTheme.typography.subtitle4,
-                            color = CareTheme.colors.gray500,
-                        )
-
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            CareChipBasic(
-                                text = Gender.WOMAN.displayName,
-                                enable = gender == Gender.WOMAN,
-                                modifier = Modifier.width(104.dp),
-                            )
-
-                            CareChipBasic(
-                                text = Gender.MAN.displayName,
-                                enable = gender == Gender.MAN,
-                                modifier = Modifier.width(104.dp),
-                            )
-                        }
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.Start,
-                        verticalArrangement = Arrangement.spacedBy(6.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 20.dp, end = 20.dp, bottom = 24.dp),
-                    ) {
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            modifier = Modifier.padding(bottom = 6.dp),
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.gender),
-                                style = CareTheme.typography.subtitle4,
-                                color = CareTheme.colors.gray500,
-                                textAlign = TextAlign.Start,
-                                modifier = Modifier.weight(1f),
-                            )
-
-                            Text(
-                                text = stringResource(id = R.string.experience),
-                                style = CareTheme.typography.subtitle4,
-                                color = CareTheme.colors.gray500,
-                                textAlign = TextAlign.Start,
-                                modifier = Modifier.weight(1f),
-                            )
-                        }
-
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            CareTextField(
-                                value = "58",
-                                onValueChanged = {},
-                                leftComponent = {
-                                    Text(
-                                        text = "세",
-                                        style = CareTheme.typography.body3,
-                                        color = CareTheme.colors.gray500,
-                                    )
-                                },
-                                modifier = Modifier.weight(1f),
-                            )
-
-                            CareTextField(
-                                value = "연차",
-                                onValueChanged = {},
-                                leftComponent = {
-                                    Image(
-                                        painter = painterResource(com.idle.designresource.R.drawable.ic_arrow_down),
-                                        contentDescription = null,
-                                    )
-                                },
-                                modifier = Modifier.weight(1f),
-                            )
-                        }
-                    }
                 }
 
                 HorizontalDivider(
                     thickness = 8.dp,
                     color = CareTheme.colors.gray050,
-                    modifier = Modifier.padding(bottom = 24.dp),
+                    modifier = Modifier.padding(vertical = 24.dp),
                 )
 
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.spacedBy(6.dp),
+                if (isEditState) {
+                    LabeledContent(
+                        subtitle = stringResource(id = R.string.experience),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 20.dp, end = 20.dp, bottom = 32.dp),
+                    ) {
+                        CareClickableTextField(
+                            value = "",
+                            hint = stringResource(id = R.string.year),
+                            leftComponent = {
+                                Image(
+                                    painter = painterResource(R.drawable.ic_arrow_down),
+                                    contentDescription = null,
+                                )
+                            },
+                            onClick = {},
+                            modifier = Modifier.fillMaxWidth(),
+                        )
+                    }
+                }
+
+                LabeledContent(
+                    subtitle = stringResource(id = R.string.address),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 20.dp, end = 20.dp, bottom = 28.dp),
+                        .padding(start = 20.dp, end = 20.dp, bottom = 32.dp),
                 ) {
-                    Text(
-                        text = stringResource(id = R.string.address),
-                        style = CareTheme.typography.subtitle4,
-                        color = CareTheme.colors.gray500,
-                    )
-
                     if (!isEditState) {
                         Text(
                             text = "서울특별시 강남구 삼성동 512-3",
@@ -465,22 +377,15 @@ internal fun WorkerProfileScreen(
                     }
                 }
 
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.spacedBy(6.dp),
+                LabeledContent(
+                    subtitle = stringResource(id = R.string.worker_introduce),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 20.dp, end = 20.dp, bottom = 28.dp),
+                        .padding(start = 20.dp, end = 20.dp, bottom = 32.dp),
                 ) {
-                    Text(
-                        text = stringResource(id = R.string.worker_introduce),
-                        style = CareTheme.typography.subtitle4,
-                        color = CareTheme.colors.gray500,
-                    )
-
                     if (!isEditState) {
                         Text(
-                            text = workerIntroduce,
+                            text = workerIntroduce.ifBlank { "-" },
                             style = CareTheme.typography.body3,
                             color = CareTheme.colors.gray900,
                         )
@@ -493,22 +398,15 @@ internal fun WorkerProfileScreen(
                     }
                 }
 
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.spacedBy(6.dp),
+                LabeledContent(
+                    subtitle = stringResource(id = R.string.specialty),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 20.dp, end = 20.dp, bottom = 28.dp),
+                        .padding(start = 20.dp, end = 20.dp, bottom = 32.dp),
                 ) {
-                    Text(
-                        text = stringResource(id = R.string.specialty),
-                        style = CareTheme.typography.subtitle4,
-                        color = CareTheme.colors.gray500,
-                    )
-
                     if (!isEditState) {
                         Text(
-                            text = specialty,
+                            text = specialty.ifBlank { "-" },
                             style = CareTheme.typography.body3,
                             color = CareTheme.colors.gray900,
                         )
