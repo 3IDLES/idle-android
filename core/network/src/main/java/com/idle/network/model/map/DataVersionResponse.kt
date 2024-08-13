559b2ed51c8c357838936b3259b492e6c9948363
tgyuuAn <atg0614@gmail.com>
2024-08-13 15:52:21 +0900

    [IDLE-277] StaticMap관련 코드 제거

--- core/network/src/main/java/com/idle/network/model/map/DataVersionResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.idle.network.model.map
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DataVersionResponse(
-    val version: String,
-    val interval: Int,
-)
--- feature/worker/job-posting-detail/src/main/java/com/idle/worker/job/posting/detail/WorkerJobPostingDetailViewModel.kt
+++ feature/worker/job-posting-detail/src/main/java/com/idle/worker/job/posting/detail/WorkerJobPostingDetailViewModel.kt
@@ -1,39 +1,8 @@
 package com.idle.worker.job.posting.detail
 
-import android.util.Log
-import androidx.lifecycle.viewModelScope
 import com.idle.binding.base.BaseViewModel
-import com.idle.domain.model.map.MapMarker
-import com.idle.domain.usecase.map.GetStaticMapUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class WorkerJobPostingDetailViewModel @Inject constructor(
-    private val getStaticMapUseCase: GetStaticMapUseCase,
-) : BaseViewModel() {
-    private val _staticMap = MutableStateFlow<ByteArray?>(null)
-    val staticMap = _staticMap.asStateFlow()
-
-    fun getStaticMap(width: Int, height: Int) = viewModelScope.launch {
-        getStaticMapUseCase(
-            width = width,
-            height = height,
-            markers = listOf(
-                MapMarker(
-                    icon = "https://idle-bucket.s3.ap-northeast-2.amazonaws.com/assets/Pin_Home.svg",
-                    pos = "128.091799 35.132965",
-                ),
-                MapMarker(
-                    icon = "https://idle-bucket.s3.ap-northeast-2.amazonaws.com/assets/Pin_Workplace.svg",
-                    pos = "128.092849 35.132968",
-                )
-            )
-        ).onSuccess { _staticMap.value = it }
-            .onFailure { Log.d("test", "지도 정보를 부르는데 실패하였습니다.") }
-    }
-
-}
+class WorkerJobPostingDetailViewModel @Inject constructor() : BaseViewModel() {}
