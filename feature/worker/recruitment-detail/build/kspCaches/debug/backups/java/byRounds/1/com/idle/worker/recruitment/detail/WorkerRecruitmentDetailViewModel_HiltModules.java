91cd51f04939d3a9ab6facd80050acacc34ff9ff
tgyuuAn <atg0614@gmail.com>
2024-08-13 15:45:15 +0900

    [IDLE-277] DynamicMap에 상호작용을 막는 속성 추가

---
+++ feature/worker/recruitment-detail/build/kspCaches/debug/backups/java/byRounds/1/com/idle/worker/recruitment/detail/WorkerRecruitmentDetailViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.idle.worker.recruitment.detail;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = WorkerRecruitmentDetailViewModel.class
+)
+public final class WorkerRecruitmentDetailViewModel_HiltModules {
+  private WorkerRecruitmentDetailViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(WorkerRecruitmentDetailViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(WorkerRecruitmentDetailViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(WorkerRecruitmentDetailViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
