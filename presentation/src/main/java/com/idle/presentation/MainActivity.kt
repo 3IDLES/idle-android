15d33e7d1b352d42520c0ed12771201d5939d3e7
tgyuuAn <atg0614@gmail.com>
2024-08-22 16:11:16 +0900

    [IDLE-000] 스플래쉬 구현

--- presentation/src/main/java/com/idle/presentation/MainActivity.kt
+++ presentation/src/main/java/com/idle/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -45,6 +46,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
