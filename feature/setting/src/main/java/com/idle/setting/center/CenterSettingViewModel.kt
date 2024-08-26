131bb321eada4e745a85ff1cd84c2248c35b4448
tgyuuAn <atg0614@gmail.com>
2024-08-27 01:29:49 +0900

    [IDLE-336] 센터 설정 화면에서도 Local에 저장되어 있는 값을 이용하도록 변경

--- feature/setting/src/main/java/com/idle/setting/center/CenterSettingViewModel.kt
+++ feature/setting/src/main/java/com/idle/setting/center/CenterSettingViewModel.kt
@@ -3,20 +3,41 @@ package com.idle.setting.center
 import androidx.lifecycle.viewModelScope
 import com.idle.binding.base.BaseViewModel
 import com.idle.binding.base.CareBaseEvent
+import com.idle.domain.model.profile.CenterProfile
 import com.idle.domain.usecase.auth.LogoutCenterUseCase
+import com.idle.domain.usecase.profile.GetLocalMyCenterProfileUseCase
 import com.idle.setting.SettingEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CenterSettingViewModel @Inject constructor(
+    private val getLocalMyCenterProfileUseCase: GetLocalMyCenterProfileUseCase,
     private val logoutCenterUseCase: LogoutCenterUseCase,
 ) : BaseViewModel() {
+    private val _centerProfile =
+        MutableStateFlow<CenterProfile>(CenterProfile("", "", "", "", "", 0.0, 0.0, "", ""))
+    val centerProfile = _centerProfile.asStateFlow()
+
     private val _centerSettingEvent = MutableSharedFlow<SettingEvent>()
     val centerSettingEvent = _centerSettingEvent.asSharedFlow()
+
+    init {
+        getMyProfile()
+    }
+
+    private fun getMyProfile() = viewModelScope.launch {
+        getLocalMyCenterProfileUseCase().onSuccess {
+            _centerProfile.value = it
+        }.onFailure {
+            baseEvent(CareBaseEvent.Error(it.message.toString()))
+        }
+    }
 
     fun logout() = viewModelScope.launch {
         logoutCenterUseCase().onSuccess {
--- feature/setting/src/main/java/com/idle/setting/worker/WorkerSettingViewModel.kt
+++ feature/setting/src/main/java/com/idle/setting/worker/WorkerSettingViewModel.kt
@@ -3,20 +3,40 @@ package com.idle.setting.worker
 import androidx.lifecycle.viewModelScope
 import com.idle.binding.base.BaseViewModel
 import com.idle.binding.base.CareBaseEvent
+import com.idle.domain.model.profile.WorkerProfile
 import com.idle.domain.usecase.auth.LogoutWorkerUseCase
+import com.idle.domain.usecase.profile.GetLocalMyWorkerProfileUseCase
 import com.idle.setting.SettingEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WorkerSettingViewModel @Inject constructor(
+    private val getLocalMyWorkerProfileUseCase: GetLocalMyWorkerProfileUseCase,
     private val logoutWorkerUseCase: LogoutWorkerUseCase,
 ) : BaseViewModel() {
+    private val _workerProfile = MutableStateFlow<WorkerProfile?>(null)
+    val workerProfile = _workerProfile.asStateFlow()
+
     private val _workerSettingEvent = MutableSharedFlow<SettingEvent>()
     val workerSettingEvent = _workerSettingEvent.asSharedFlow()
+
+    init {
+        getMyProfile()
+    }
+
+    private fun getMyProfile() = viewModelScope.launch {
+        getLocalMyWorkerProfileUseCase().onSuccess {
+            _workerProfile.value = it
+        }.onFailure {
+            baseEvent(CareBaseEvent.Error(it.message.toString()))
+        }
+    }
 
     fun logout() = viewModelScope.launch {
         logoutWorkerUseCase().onSuccess {
