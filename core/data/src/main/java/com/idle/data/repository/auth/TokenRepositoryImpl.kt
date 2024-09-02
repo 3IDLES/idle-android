df259f36a05f118dc1742ad3678d65e33e1fce47
tgyuuAn <atg0614@gmail.com>
2024-09-02 19:41:44 +0900

    [IDLE-000] DataStore에서 IO작업이 일어나는 부분에서 Dispatchers.IO 적용

--- core/data/src/main/java/com/idle/data/repository/auth/TokenRepositoryImpl.kt
+++ core/data/src/main/java/com/idle/data/repository/auth/TokenRepositoryImpl.kt
@@ -2,11 +2,15 @@ package com.idle.data.repository.auth
 
 import com.idle.datastore.datasource.TokenDataSource
 import com.idle.domain.repositorry.auth.TokenRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class TokenRepositoryImpl @Inject constructor(
     private val tokenDataSource: TokenDataSource,
 ) : TokenRepository {
-    override suspend fun getAccessToken(): String = tokenDataSource.accessToken.first()
+    override suspend fun getAccessToken(): String = withContext(Dispatchers.IO) {
+        tokenDataSource.accessToken.first()
+    }
 }
