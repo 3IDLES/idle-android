20d644f9ffc81b35a2b8ac752607f40826aba59b
tgyuuAn <atg0614@gmail.com>
2024-09-04 16:19:52 +0900

    [IDLE-344] CareTextToggle Preview 추가

--- core/designsystem/compose/src/main/java/com/idle/designsystem/compose/component/Toggle.kt
+++ core/designsystem/compose/src/main/java/com/idle/designsystem/compose/component/Toggle.kt
@@ -18,15 +18,21 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.idle.compose.clickable
+import com.idle.designsystem.compose.FLIP
 import com.idle.designsystem.compose.foundation.CareTheme
 
 @Composable
@@ -104,4 +110,40 @@ fun CareToggleText(
             )
         }
     }
+}
+
+@Composable
+private fun CareToggleTextContent() {
+    var isChecked by remember { mutableStateOf(false) }
+
+    CareToggleText(
+        rightChecked = isChecked,
+        leftText = "왼쪽",
+        rightText = "오른쪽",
+        onCheckedChanged = { newCheckedState -> isChecked = newCheckedState },
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+@Preview(name = "CareToggleText_Default", showBackground = true, group = "Default")
+@Composable
+private fun PreviewCareToggleTextDefault() {
+    CareToggleTextContent()
+}
+
+@Preview(name = "CareToggleText_Flip", showBackground = true, device = FLIP, group = "Flip")
+@Composable
+private fun PreviewCareToggleTextFlip() {
+    CareToggleTextContent()
+}
+
+@Preview(
+    name = "CareToggleText_Foldable",
+    showBackground = true,
+    device = Devices.FOLDABLE,
+    group = "Fold"
+)
+@Composable
+private fun PreviewCareToggleTextFoldable() {
+    CareToggleTextContent()
 }
