1b21135e300d50ea7a4e41bdefbcf1f2a9f5a532
tgyuuAn <atg0614@gmail.com>
2024-09-07 17:57:29 +0900

    [IDLE-353] Firebase Remote Config 의존성 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -31,4 +31,5 @@ dependencies {
     implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.okhttp.logging)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.firebase.config)
 }
