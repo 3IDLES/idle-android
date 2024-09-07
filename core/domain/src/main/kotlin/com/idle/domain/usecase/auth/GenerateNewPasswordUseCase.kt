2720cf2b0c26d92bc67dff77bc24aac17cdeac4d
tgyuuAn <atg0614@gmail.com>
2024-09-07 17:02:37 +0900

    [IDLE-000] Domain Util 추가

--- core/domain/src/main/kotlin/com/idle/domain/usecase/auth/GenerateNewPasswordUseCase.kt
+++ core/domain/src/main/kotlin/com/idle/domain/usecase/auth/GenerateNewPasswordUseCase.kt
@@ -1,11 +1,18 @@
 package com.idle.domain.usecase.auth
 
 import com.idle.domain.repositorry.auth.AuthRepository
+import com.idle.domain.util.formatPhoneNumber
 import javax.inject.Inject
 
 class GenerateNewPasswordUseCase @Inject constructor(
     private val authRepository: AuthRepository,
 ) {
-    suspend operator fun invoke(newPassword: String, phoneNumber: String) =
-        authRepository.generateNewPassword(newPassword = newPassword, phoneNumber = phoneNumber)
+    suspend operator fun invoke(newPassword: String, phoneNumber: String): Result<Unit> {
+        val formattedPhoneNumber = formatPhoneNumber(phoneNumber)
+
+        return authRepository.generateNewPassword(
+            newPassword = newPassword,
+            phoneNumber = formattedPhoneNumber
+        )
+    }
 }
