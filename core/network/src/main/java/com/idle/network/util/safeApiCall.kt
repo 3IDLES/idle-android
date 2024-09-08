780a43cbed3453dee4212e03d54cc2a0ca1677ea
tgyuuAn <atg0614@gmail.com>
2024-09-08 18:52:30 +0900

    [IDLE-000] 불필요한 코드 정리

--- core/network/src/main/java/com/idle/network/util/safeApiCall.kt
+++ core/network/src/main/java/com/idle/network/util/safeApiCall.kt
@@ -3,6 +3,8 @@ package com.idle.network.util
 import com.idle.domain.model.error.ApiErrorCode
 import com.idle.domain.model.error.HttpResponseException
 import com.idle.domain.model.error.HttpResponseStatus
+import com.idle.network.model.error.ErrorResponse
+import kotlinx.serialization.json.Json
 import retrofit2.Response
 
 internal inline fun <T> safeApiCall(apiCall: () -> Response<T>): Result<T> {
@@ -14,6 +16,32 @@ internal inline fun <T> safeApiCall(apiCall: () -> Response<T>): Result<T> {
             HttpResponseException(
                 status = HttpResponseStatus.create(-1),
                 apiErrorCode = ApiErrorCode.NetworkError
+            )
+        )
+    }
+}
+
+internal fun <T> Response<T>.onResponse(): Result<T> {
+    if (isSuccessful) {
+        body()?.let {
+            return Result.success(it)
+        } ?: return Result.success(Unit as T)
+    } else {
+        errorBody()?.let {
+            val json = Json { ignoreUnknownKeys = true }
+            val errorResponse = json.decodeFromString<ErrorResponse>(it.string())
+
+            return Result.failure(
+                HttpResponseException(
+                    status = HttpResponseStatus.create(code()),
+                    apiErrorCode = ApiErrorCode.create(errorResponse.code),
+                    msg = errorResponse.message,
+                )
+            )
+        } ?: return Result.failure(
+            HttpResponseException(
+                status = HttpResponseStatus.create(-1),
+                apiErrorCode = ApiErrorCode.UnknownError
             )
         )
     }
