3a1ea0f1277f2f3e7825a66429be0c5ccc7ce4fb
tgyuuAn <atg0614@gmail.com>
2024-09-09 16:21:13 +0900

    [IDLE-359] 센터 프로필 등록 상태에 따른 화면 분기

--- feature/signin/src/main/java/com/idle/signin/center/CenterSignInViewModel.kt
+++ feature/signin/src/main/java/com/idle/signin/center/CenterSignInViewModel.kt
@@ -5,15 +5,17 @@ import com.idle.analytics.AnalyticsEvent
 import com.idle.analytics.AnalyticsEvent.PropertiesKeys.ACTION_NAME
 import com.idle.analytics.AnalyticsEvent.PropertiesKeys.ACTION_RESULT
 import com.idle.analytics.helper.AnalyticsHelper
-import com.idle.binding.DeepLinkDestination
 import com.idle.binding.DeepLinkDestination.CenterHome
 import com.idle.binding.DeepLinkDestination.CenterPending
+import com.idle.binding.DeepLinkDestination.CenterRegister
 import com.idle.binding.base.BaseViewModel
 import com.idle.binding.base.CareBaseEvent.NavigateTo
+import com.idle.domain.model.error.ApiErrorCode
 import com.idle.domain.model.error.HttpResponseException
 import com.idle.domain.model.profile.CenterManagerAccountStatus
 import com.idle.domain.usecase.auth.SignInCenterUseCase
 import com.idle.domain.usecase.profile.GetCenterStatusUseCase
+import com.idle.domain.usecase.profile.GetMyCenterProfileUseCase
 import com.idle.signin.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -25,6 +27,7 @@ import javax.inject.Inject
 class CenterSignInViewModel @Inject constructor(
     private val signInCenterUseCase: SignInCenterUseCase,
     private val getCenterStatusUseCase: GetCenterStatusUseCase,
+    private val getMyCenterProfileUseCase: GetMyCenterProfileUseCase,
     private val analyticsHelper: AnalyticsHelper,
 ) : BaseViewModel() {
     private val _centerId = MutableStateFlow("")
@@ -45,34 +48,51 @@ class CenterSignInViewModel @Inject constructor(
         signInCenterUseCase(identifier = _centerId.value, password = _centerPassword.value)
             .onSuccess {
                 analyticsHelper.setUserId(_centerId.value)
-                getCenterStatus()
+                handleCenterLoginSuccess()
             }
-            .onFailure {
-                handleFailure(it as HttpResponseException)
-
-                analyticsHelper.logEvent(
-                    AnalyticsEvent(
-                        type = AnalyticsEvent.Types.ACTION,
-                        properties = mutableMapOf(
-                            ACTION_NAME to "center_login",
-                            ACTION_RESULT to false,
-                        )
-                    )
-                )
+            .onFailure { exception ->
+                handleLoginFailure(exception as HttpResponseException)
             }
     }
 
-    private fun getCenterStatus() = viewModelScope.launch {
-        getCenterStatusUseCase().onSuccess {
-            when (it.centerManagerAccountStatus) {
-                CenterManagerAccountStatus.APPROVED ->
-                    baseEvent(NavigateTo(CenterHome, R.id.centerSignInFragment))
-
-                else ->
-                    baseEvent(NavigateTo(CenterPending, R.id.centerSignInFragment))
-            }
-        }.onFailure {
-            handleFailure(it as HttpResponseException)
+    private fun handleCenterLoginSuccess() = viewModelScope.launch {
+        getCenterStatusUseCase().onSuccess { centerStatusResponse ->
+            navigateBasedOnCenterStatus(centerStatusResponse.centerManagerAccountStatus)
+        }.onFailure { exception ->
+            handleFailure(exception as HttpResponseException)
         }
+    }
+
+    private fun navigateBasedOnCenterStatus(status: CenterManagerAccountStatus) {
+        when (status) {
+            CenterManagerAccountStatus.APPROVED -> fetchAndNavigateToProfile()
+            else -> baseEvent(NavigateTo(CenterPending, R.id.centerSignInFragment))
+        }
+    }
+
+    private fun fetchAndNavigateToProfile() = viewModelScope.launch {
+        getMyCenterProfileUseCase().onSuccess {
+            baseEvent(NavigateTo(CenterHome, R.id.centerSignInFragment))
+        }.onFailure {
+            val error = it as HttpResponseException
+            if (error.apiErrorCode == ApiErrorCode.CenterNotFound) {
+                baseEvent(NavigateTo(CenterRegister, R.id.centerSignInFragment))
+            } else {
+                handleFailure(error)
+            }
+        }
+    }
+
+    private fun handleLoginFailure(error: HttpResponseException) {
+        handleFailure(error)
+        analyticsHelper.logEvent(
+            AnalyticsEvent(
+                type = AnalyticsEvent.Types.ACTION,
+                properties = mutableMapOf(
+                    ACTION_NAME to "center_login",
+                    ACTION_RESULT to false,
+                )
+            )
+        )
     }
 }
