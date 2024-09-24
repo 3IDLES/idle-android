daeb265dd6923cf4f5e7af4c5dd1ee51bb057c01
tgyuuAn <atg0614@gmail.com>
2024-09-24 14:43:04 +0900

    [IDLE-377] onNewToken이 호출될 때 만약 로그인된 상태라면 서버로 디바이스 토큰을 전송하도록 변경

--- app/src/main/java/com/idle/care/notification/NotificationService.kt
+++ app/src/main/java/com/idle/care/notification/NotificationService.kt
@@ -3,26 +3,61 @@ package com.idle.care.notification
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.idle.domain.model.auth.UserType
+import com.idle.domain.usecase.auth.GetUserRoleUseCase
+import com.idle.domain.usecase.auth.PutDeviceTokenUseCase
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class NotificationService : FirebaseMessagingService() {
 
     @Inject
+    lateinit var putDeviceTokenUseCase: PutDeviceTokenUseCase
+
+    @Inject
+    lateinit var getUserRoleUseCase: GetUserRoleUseCase
+
+    @Inject
     lateinit var notificationHandler: NotificationHandler
+
+    private val job = SupervisorJob()
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.e("test", throwable.stackTraceToString())
+    }
+    private val scope = CoroutineScope(Dispatchers.IO + job + coroutineExceptionHandler)
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Log.d("token", token)
+
+        scope.launch {
+            val userRole = getUserRoleUseCase()
+
+            when (userRole) {
+                UserType.CENTER.apiValue, UserType.WORKER.apiValue -> putDeviceTokenUseCase(token)
+            }
+        }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
+        val title = message.notification?.title ?: message.data["title"] ?: ""
+        val body = message.notification?.body ?: message.data["body"] ?: ""
+
         notificationHandler.deliverNotification(
-            title = message.notification?.title ?: "",
-            body = message.notification?.body ?: "",
+            title = title,
+            body = body,
         )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
