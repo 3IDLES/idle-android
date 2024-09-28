d244866e76bb37a266c5ace2ccdb21d1022bb5d8
tgyuuAn <atg0614@gmail.com>
2024-09-28 17:18:58 +0900

    [IDLE-395] 디바이스 토큰 제거 로직을 토큰이 제거되기 이전에 수행하도록 변경

--- feature/notification/src/main/java/com/idle/notification/NotificationFragment.kt
+++ feature/notification/src/main/java/com/idle/notification/NotificationFragment.kt
@@ -1,16 +1,25 @@
 package com.idle.notification
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
@@ -31,6 +40,7 @@ internal class NotificationFragment : BaseComposeFragment() {
         fragmentViewModel.apply {
             NotificationScreen(
                 snackbarHostState = snackbarHostState,
+                notificationList = listOf("", "", "", "", "", ""),
                 navigateUp = { findNavController().navigateUp() },
             )
         }
@@ -40,6 +50,7 @@ internal class NotificationFragment : BaseComposeFragment() {
 @Composable
 private fun NotificationScreen(
     snackbarHostState: SnackbarHostState,
+    notificationList: List<String>,
     navigateUp: () -> Unit,
 ) {
     Scaffold(
@@ -70,8 +81,67 @@ private fun NotificationScreen(
                 .fillMaxSize()
                 .background(CareTheme.colors.white000)
                 .padding(paddingValue)
-                .padding(start = 20.dp, end = 20.dp, top = 24.dp),
+                .padding(top = 24.dp),
         ) {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                item {
+                    Text(
+                        text = "오늘",
+                        style = CareTheme.typography.subtitle2,
+                        color = CareTheme.colors.gray900,
+                        modifier = Modifier.padding(start = 20.dp, top = 24.dp, bottom = 8.dp),
+                    )
+                }
+
+                items(items = notificationList) { notification ->
+                    NotificationItem()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun NotificationItem(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.Top,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .background(CareTheme.colors.gray100)
+                    .clip(CircleShape)
+            )
+
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = "30분 전",
+                    style = CareTheme.typography.caption,
+                    color = CareTheme.colors.gray500,
+                    modifier = Modifier.padding(bottom = 2.dp),
+                )
+
+                Text(
+                    text = "김철수 님이 공고에 지원하였습니다.",
+                    style = CareTheme.typography.subtitle3,
+                    color = CareTheme.colors.gray900,
+                    modifier = Modifier.padding(bottom = 1.dp),
+                )
+
+                Text(
+                    text = "서울특별시 강남구 신사동 1등급 78세 여성",
+                    style = CareTheme.typography.body3,
+                    color = CareTheme.colors.gray300,
+                )
+            }
         }
     }
 }
