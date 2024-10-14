75816bd6fc129f8c3968a695fb574981023b6ca9
tgyuuAn <atg0614@gmail.com>
2024-10-14 15:03:14 +0900

    [IDLE-414] ServcerDriven Notification Domain Model 정의

--- core/domain/src/main/kotlin/com/idle/domain/model/notification/Notification.kt
+++ core/domain/src/main/kotlin/com/idle/domain/model/notification/Notification.kt
@@ -1,5 +1,34 @@
 package com.idle.domain.model.notification
 
+import java.lang.reflect.Type
+import java.time.LocalDateTime
+
 data class Notification(
-    val something: String,
+    val notificationType: NotificationType,
+    val id: String,
+    val isRead: Boolean,
+    val title: String,
+    val body: String,
+    val createdAt: LocalDateTime,
+    val notificationDetails: NotificationContent,
 )
+
+enum class NotificationType(private val notificationTypeClass: Type) {
+    APPLICANT(NotificationContent.ApplicantNotification::class.java),
+    UNKNOWN(NotificationContent.UnKnownNotification::class.java);
+
+    companion object {
+        fun create(notificationType: String?): NotificationType {
+            return NotificationType.entries.firstOrNull { it.name == notificationType } ?: UNKNOWN
+        }
+
+        fun findNotificationClassByType(notificationType: NotificationType): Type {
+            return notificationType.notificationTypeClass
+        }
+    }
+}
+
+sealed class NotificationContent {
+    data class ApplicantNotification(val jobPostingId: String) : NotificationContent()
+    data object UnKnownNotification : NotificationContent()
+}
