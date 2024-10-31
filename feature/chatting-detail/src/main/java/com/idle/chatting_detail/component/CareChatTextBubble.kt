dbf2be03ebf94c50b36ab3c6862728d6a5e8bd1d
tgyuuAn <atg0614@gmail.com>
2024-10-31 14:45:17 +0900

    [IDLE-490] 채팅방 읽지 않은 메세지 컴포넌트 추가

--- feature/chatting-detail/src/main/java/com/idle/chatting_detail/component/CareChatTextBubble.kt
+++ feature/chatting-detail/src/main/java/com/idle/chatting_detail/component/CareChatTextBubble.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -100,7 +101,7 @@ fun CareChatSenderTextBubbleWithImage(
                 horizontalAlignment = Alignment.Start,
                 modifier = Modifier
                     .padding(start = 4.dp)
-                    .width(35.dp)
+                    .wrapContentWidth()
                     .align(Alignment.Bottom),
             ) {
                 if (isRead) {
