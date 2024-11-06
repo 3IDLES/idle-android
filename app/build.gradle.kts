90e185e0c19312b988b1da13bd7cf95b8b8d05f6
tgyuuAn <atg0614@gmail.com>
2024-11-06 18:08:55 +0900

    [IDLE-501] 센터 인증대기 화면 Logging 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "com.idle.care"
 
     defaultConfig {
-        versionCode = 9
-        versionName = "1.1.2"
+        versionCode = 10
+        versionName = "1.1.3"
         targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
