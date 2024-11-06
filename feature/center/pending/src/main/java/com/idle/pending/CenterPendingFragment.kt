3fa71e9711fdbf2d9d465e96f567d188717ad61e
tgyuuAn <atg0614@gmail.com>
2024-11-06 18:08:55 +0900

    [IDLE-501] 센터 인증대기 화면 Logging 추가

--- feature/center/pending/src/main/java/com/idle/pending/CenterPendingFragment.kt
+++ feature/center/pending/src/main/java/com/idle/pending/CenterPendingFragment.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.fragment.navArgs
+import com.idle.analytics.businessmetric.TrackScreenViewEvent
 import com.idle.center.pending.R
 import com.idle.compose.base.BaseComposeFragment
 import com.idle.compose.clickable
@@ -172,6 +173,8 @@ private fun CenterPendingScreen(
             )
         }
     }
+
+    TrackScreenViewEvent(screenName = "center_pending_screen")
 }
 
 @Composable
