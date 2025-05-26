b85bfe93c86ff6cacac32ed44452f5c443bae374
tgyuuAn <atg0614@gmail.com>
2025-05-26 16:00:50 +0900

    [IDLE-000] 서버에서 메시지를 가져올 때, 온전히 UI에 보여주지 않던 버그 수정

--- core/data/src/main/java/com/idle/data/repository/ChatRepositoryImpl.kt
+++ core/data/src/main/java/com/idle/data/repository/ChatRepositoryImpl.kt
@@ -80,9 +80,7 @@ class ChatRepositoryImpl @Inject constructor(
         myId: String,
         messageId: String?,
     ): Result<List<ChatMessage>> = runCatching {
-        if (messageId == null ||
-            !localChatDataSource.isMessageExist(roomId, myId, messageId)
-        ) {
+        if (messageId == null || !localChatDataSource.isMessageExist(roomId, myId, messageId)) {
             // 웹소켓으로 얻지 못한 메세지가 있을경우 정합성을 위해 서버에서 호출
             when (userType) {
                 UserType.WORKER -> chatDataSource.getWorkerChatRoomMessages(
@@ -96,20 +94,10 @@ class ChatRepositoryImpl @Inject constructor(
                 )
             }.mapCatching { response ->
                 val messages = response.chatMessageInfos
-                val readSequence = response.sequence
-
-                messages.lastOrNull()?.let {
-                    localChatDataSource.readMessages(
-                        roomId = roomId,
-                        myId = myId,
-                        senderId = it.senderId ?: return@let,
-                        sequence = readSequence,
-                    )
-                }
 
                 messages
                     .sortedBy { it.sequence }
-                    .map {
+                    .forEach {
                         val message = it.toVO()
 
                         if (!localChatDataSource.isChatRoomExist(message.roomId, myId)) {
@@ -131,6 +119,18 @@ class ChatRepositoryImpl @Inject constructor(
                             localChatDataSource.insertMessage(message, myId)
                         }
                     }
+
+                val readSequence = response.sequence
+                messages.lastOrNull()?.let {
+                    localChatDataSource.readMessages(
+                        roomId = roomId,
+                        myId = myId,
+                        senderId = it.senderId ?: return@let,
+                        sequence = readSequence,
+                    )
+                }
+
+                messages
             }.getOrThrow()
         }
 
